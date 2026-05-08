877921b6ee1cb0cfb534853244356ae62cf949b0
YOBRA-coder <56080569+YOBRA-coder@users.noreply.github.com>
2026-05-08 03:15:07 +0300

    Update database.ts

--- src/config/database.ts
+++ src/config/database.ts
@@ -21,7 +21,7 @@ const poolConfig: PoolConfig = {
 };*/
 
 export const pool = new Pool({
-  connectionString:process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
   connectionTimeoutMillis: 5000,
 });
